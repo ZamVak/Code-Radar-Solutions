03933696f5ba918b41532f3442e799aac77ccc13
Zamaam Vakeel <150921399+ZamVak@users.noreply.github.com>
2025-02-06 21:06:05 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,12 +4,16 @@ int main() {
     int a,b;
     char opr;
     scanf("%d %d %c",&a,&b,&opr);
+    if(opr=='/' && b==0){
+        printf("error");
+    }
+    else{
     switch(opr){
         case '+':printf("%d",a+b);break;
         case '-':printf("%d",a-b);break;
         case '*':printf("%d",a*b);break;
-        //case '/':printf("%f",a/b);break;
-        default:printf("Invalid");
-    }
+        case '/':printf("%f",a/b);break;
+        default:printf("error");
+    }}
     return 0;
 }
