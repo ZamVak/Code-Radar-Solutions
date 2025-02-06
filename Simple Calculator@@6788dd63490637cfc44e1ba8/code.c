11f6c3d4c888c2031035c1e87b246da4412a4c39
Zamaam Vakeel <150921399+ZamVak@users.noreply.github.com>
2025-02-06 20:58:36 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,7 +3,7 @@
 int main() {
     int a,b;
     char opr;
-    scanf("%d%d%c",&a,&b,&opr);
+    scanf("%d %d %c",&a,&b,&opr);
     switch(day){
         case '+':printf("%d",a+b);break;
         case '-':printf("%d",a-b);break;
